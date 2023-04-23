e50a4a4a2bc42f5fc305df88b8663050aae7700c
ManitDave <manit.dave@outlook.com>
2023-04-23 12:41:53 +0530

    Updated

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
+	<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/assets/images/logo.ico" type="image/x-icon">
@@ -13,11 +12,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/style.css">
-    <title>Document</title>
+	<title>Document</title>
 </head>
-
-<body> 
-    <div class="img">
+<body>
+	<div class="img">
         <a href="http://www.rcti.cteguj.in/" target="_blank">
             <img src="/assets/images/logo.jpg" alt="Logo" height="100px" width="100px">
         </a>
@@ -49,113 +47,6 @@
                 </select>
             </div>
         </div>
-    </div>
-    <div class="frm">
-        <div class="questions">
-            <div class="qu1">
-                <p class="question">What is your favorite color?</p>
-                <div class="answer-options">
-                    <label>
-                        <input type="radio" name="feedback" value="very-good" id="very-good">
-                        Very Good
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="good" id="good">
-                        Good
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="ok" id="ok">
-                        OK
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="bad" id="bad">
-                        Bad
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="very-bad" id="very-bad">
-                        Very Bad
-                    </label>
-                </div>
-            </div>
-            <div class="qu2">
-                <p class="question">What is your favorite color?</p>
-                <div class="answer-options">
-                    <label>
-                        <input type="radio" name="feedback" value="very-good" id="very-good">
-                        Very Good
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="good" id="good">
-                        Good
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="ok" id="ok">
-                        OK
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="bad" id="bad">
-                        Bad
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="very-bad" id="very-bad">
-                        Very Bad
-                    </label>
-                </div>
-            </div>
-            <div class="qu3">
-                <p class="question">What is your favorite color?</p>
-                <div class="answer-options">
-                    <label>
-                        <input type="radio" name="feedback" value="very-good" id="very-good">
-                        Very Good
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="good" id="good">
-                        Good
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="ok" id="ok">
-                        OK
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="bad" id="bad">
-                        Bad
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="very-bad" id="very-bad">
-                        Very Bad
-                    </label>
-                </div>
-            </div>
-            <div class="qu4">
-                <p class="question">What is your favorite color?</p>
-                <div class="answer-options">
-                    <label>
-                        <input type="radio" name="feedback" value="very-good" id="very-good">
-                        Very Good
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="good" id="good">
-                        Good
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="ok" id="ok">
-                        OK
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="bad" id="bad">
-                        Bad
-                    </label>
-                    <label>
-                        <input type="radio" name="feedback" value="very-bad" id="very-bad">
-                        Very Bad
-                    </label>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="submit">
-        <button>Submit</button>
     </div>
     <script src="/js/app.js"></script>
 </body>
